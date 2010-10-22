da339c3fade2f7a062280c205c79e6ab5fa59114
bpodgursky <bpodgursky@214279a3-1979-847b-5814-d831109a3ab6>
2010-10-22 19:14:22 +0000

    added in some move logic and order classes

--- src/gamestate/Unit.java
+++ src/gamestate/Unit.java
@@ -3,13 +3,16 @@ package gamestate;
 
 public class Unit {
 	
-	final Player belongsTo;
-	final boolean army; //false is fleet
+	public final Player belongsTo;
+	public final boolean army; //false is fleet
 
 	public Unit(Player belongsTo, boolean isArmy){
 		
 		this.belongsTo = belongsTo;
 		this.army = isArmy;
-		
+	}
+	
+	public String toString(){
+		return (army)?"A ":"F "+belongsTo.getName();
 	}
 }
