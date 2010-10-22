798ba6f9d4b22bc4ff9f977bb030a8a7e6461006
bpodgursky <bpodgursky@214279a3-1979-847b-5814-d831109a3ab6>
2010-10-22 19:14:46 +0000

    class I forgot to add

---
+++ trunk/src/order/Convoy.java
@@ -0,0 +1,16 @@
+package order;
+
+import gamestate.Unit;
+
+public class Convoy {
+
+	public final Unit convoyedUnit;
+	public final Unit convoyingUnit;
+	
+	public final TerritorySquare convoyer;
+	public final TerritorySquare from;
+	public final TerritorySquare to;
+	
+	public Convoy()//TODO finishing
+	
+}
